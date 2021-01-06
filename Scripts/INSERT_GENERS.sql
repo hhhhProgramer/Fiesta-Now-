ec58fd214615b6f5b6d8ab253cc9c4073710f432
hhhh <you@example.com>
2021-01-06 15:59:32 -0600

    Scripts

---
+++ Scripts/INSERT_GENERS.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO CodigoBailes VALUES(
+    'Baile recreativo',
+    'Ballet'
+),
+(
+    'Baile recreativo',
+    'Zumba'
+),
+(
+    'Baile recreativo',
+    'Salsa'
+),
+(
+    'Baile recreativo',
+    'Cumbia'
+)
